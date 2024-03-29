ba0ce2eeaae545e6444ca5553fae3a8115aa642c
mcrkgus <mcrkgus@naver.com>
2024-03-29 15:38:19 +0900

    [Setting] Storyboard 삭제

---
+++ DaangnMarket/DaangnMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaangnMarket
+//
+//  Created by Gahyun Kim on 2024/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
