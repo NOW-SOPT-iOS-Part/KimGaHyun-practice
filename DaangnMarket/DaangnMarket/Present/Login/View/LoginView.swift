17cf157436e4030ba6b84441d63644fb9da69fb2
mcrkgus <mcrkgus@naver.com>
2024-03-30 15:31:19 +0900

    View Setting

--- DaangnMarket/DaangnMarket/Present/Login/View/LoginView.swift
+++ DaangnMarket/DaangnMarket/Present/Login/View/LoginView.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UIKit
+
+final class LoginView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+//    private let titleLabel = UILabel()
+//    private let idTextField = UITextField()
+//    private let passwordTextField = UITextField()
+//    private lazy var loginButton = UIButton()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+// MARK: - extension
+
+private extension LoginView {
+    
+    func setupStyle() {
+        
+    }
+    
+    func setupHierarchy() {
+        
+    }
+    
+    func setupLayout() {
+        
+    }
+    
+}
--- DaangnMarket/DaangnMarket/Present/Welcome/View/WelcomeView.swift
+++ DaangnMarket/DaangnMarket/Present/Welcome/View/WelcomeView.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import UIKit
+
+final class WelcomeView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+// MARK: - extension
+
+private extension WelcomeView {
+    
+    func setupStyle() {
+        
+    }
+    
+    func setupHierarchy() {
+        
+    }
+    
+    func setupLayout() {
+        
+    }
+    
+}
