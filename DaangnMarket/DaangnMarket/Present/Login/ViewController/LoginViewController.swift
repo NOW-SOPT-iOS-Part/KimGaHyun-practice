6db112efbdd388441d5c83d07fe38478623c62ae
mcrkgus <mcrkgus@naver.com>
2024-03-30 13:58:54 +0900

    Login

--- DaangnMarket/DaangnMarket/Present/Login/ViewController/LoginViewController.swift
+++ DaangnMarket/DaangnMarket/Present/Login/ViewController/LoginViewController.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
+        label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = UIFont(name: "Pretendard-Bold", size: 18)
+        return label
+    }()
+    
+    private let idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디를 입력해주세요"
+        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호를 입력해주세요"
+        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    private func presentToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController()
+        welcomeViewController.modalPresentationStyle = .formSheet
+//        welcomeViewController.id = idTextField.text
+        welcomeViewController.setLabelText(id: idTextField.text)
+        self.present(welcomeViewController, animated: true)
+    }
+    
+    private func pushToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController()
+//        welcomeViewController.id = idTextField.text
+        welcomeViewController.setLabelText(id: idTextField.text)
+        self.navigationController?.pushViewController(welcomeViewController, animated: true)
+    }
+    
+    @objc
+    private func loginButtonDidTap() {
+//        presentToWelcomeVC()
+        pushToWelcomeVC()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        setLayout()
+    }
+    
+    private func setLayout() {
+        [titleLabel, idTextField, passwordTextField, loginButton].forEach {
+            self.view.addSubview($0)
+        }
+    }
+}
+
