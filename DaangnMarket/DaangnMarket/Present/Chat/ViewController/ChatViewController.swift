c98c822f9d641b5ac28dc3c0b0812584c5f53e72
mcrkgus <mcrkgus@naver.com>
2024-04-20 14:41:58 +0900

    [Add] #WEEK3 - Chat 폴더 추가

---
+++ DaangnMarket/DaangnMarket/Present/Chat/ViewController/ChatViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ChatViewController.swift
+//  DaangnMarket
+//
+//  Created by Gahyun Kim on 2024/04/20.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ChatViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+
+    
+    //MARK: - UI Components
+    
+ 
+    
+    //MARK: - Life Cycle
+    
+    override func loadView() {
+  
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
