eee0bdb58d6f4f9a4fc5265aafacc0aa3aef4906
mcrkgus <mcrkgus@naver.com>
2024-05-03 16:37:28 +0900

    [Chore] #WEEK4 - VC View 분리

--- DaangnMarket/DaangnMarket/Present/CheckUserInfo/CheckUserInfoView.swift
+++ DaangnMarket/DaangnMarket/Present/CheckUserInfo/CheckUserInfoView.swift
@@ -5,4 +5,77 @@
 //  Created by Gahyun Kim on 2024/05/03.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+import Moya
+
+final class CheckUserInfoView: UIView {
+    
+    let logoImageView = UIImageView(image: .imgMymelody)
+    let idLabel = UILabel()
+    let nickNameLabel = UILabel()
+    let phoneNumberLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension CheckUserInfoView {
+    func setupStyle() {
+        self.backgroundColor = .white
+        
+        idLabel.do {
+            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+            $0.textAlignment = .center
+        }
+        
+        nickNameLabel.do {
+            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+            $0.textAlignment = .center
+        }
+        
+        phoneNumberLabel.do {
+            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+            $0.textAlignment = .center
+        }
+    }
+    
+    func setupHierarchy() {
+        [logoImageView, idLabel, nickNameLabel, phoneNumberLabel].forEach {
+            self.addSubview($0)
+        }
+    }
+    
+    func setupLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(87)
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(150)
+        }
+        
+        idLabel.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(58)
+            $0.centerX.equalToSuperview()
+        }
+        nickNameLabel.snp.makeConstraints {
+            $0.top.equalTo(idLabel.snp.bottom).offset(23)
+            $0.centerX.equalToSuperview()
+        }
+        phoneNumberLabel.snp.makeConstraints {
+            $0.top.equalTo(nickNameLabel.snp.bottom).offset(23)
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
--- DaangnMarket/DaangnMarket/Present/CheckUserInfo/CheckUserInfoViewController.swift
+++ DaangnMarket/DaangnMarket/Present/CheckUserInfo/CheckUserInfoViewController.swift
@@ -9,16 +9,15 @@ import Foundation
 import UIKit
 
 final class CheckUserInfoViewController: UIViewController {
-    private let logoImageView = UIImageView(image: .imgMymelody)
-    private let idLabel = UILabel()
-    private let nickNameLabel = UILabel()
-    private let phoneNumberLabel = UILabel()
+
+    private let checkUserInfoView = CheckUserInfoView()
+    
+    override func loadView() {
+        self.view = checkUserInfoView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setStyle()
-        setLayout()
         requestUserInfo()
     }
 
@@ -29,9 +28,9 @@ final class CheckUserInfoViewController: UIViewController {
             case .success(let data):
                 guard let data = data as? UserInfoResponseModel else {
                     return }
-                self?.idLabel.text = data.data.authenticationId
-                self?.nickNameLabel.text = data.data.nickname
-                self?.phoneNumberLabel.text = data.data.phone
+                self?.checkUserInfoView.idLabel.text = data.data.authenticationId
+                self?.checkUserInfoView.nickNameLabel.text = data.data.nickname
+                self?.checkUserInfoView.phoneNumberLabel.text = data.data.phone
             case .requestErr:
                 print("요청 오류 입니다")
             case .decodedErr:
@@ -43,50 +42,6 @@ final class CheckUserInfoViewController: UIViewController {
             case .networkFail:
                 print("네트워크 오류입니다")
             }
-        }
-    }
-    
-    private func setStyle() {
-        self.view.backgroundColor = .white
-        
-        idLabel.do {
-            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
-            $0.textAlignment = .center
-        }
-        
-        nickNameLabel.do {
-            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
-            $0.textAlignment = .center
-        }
-        
-        phoneNumberLabel.do {
-            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
-            $0.textAlignment = .center
-        }
-    }
-    
-    private func setLayout() {
-        [logoImageView, idLabel, nickNameLabel, phoneNumberLabel].forEach {
-            self.view.addSubview($0)
-        }
-        
-        logoImageView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(87)
-            $0.centerX.equalToSuperview()
-            $0.size.equalTo(150)
-        }
-        
-        idLabel.snp.makeConstraints {
-            $0.top.equalTo(logoImageView.snp.bottom).offset(58)
-            $0.centerX.equalToSuperview()
-        }
-        nickNameLabel.snp.makeConstraints {
-            $0.top.equalTo(idLabel.snp.bottom).offset(23)
-            $0.centerX.equalToSuperview()
-        }
-        phoneNumberLabel.snp.makeConstraints {
-            $0.top.equalTo(nickNameLabel.snp.bottom).offset(23)
-            $0.centerX.equalToSuperview()
         }
     }
 }
